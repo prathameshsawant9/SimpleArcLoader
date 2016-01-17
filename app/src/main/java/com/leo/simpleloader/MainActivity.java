a308305954583352db0141628eafdfb1090246c3
prathamesh <leo.studio89@gmail.com>
2016-01-17 13:20:57 +0530

    Initial commit

--- app/src/main/java/com/leo/simpleloader/MainActivity.java
+++ app/src/main/java/com/leo/simpleloader/MainActivity.java
@@ -1,5 +1,6 @@
 package com.leo.simpleloader;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,10 +17,17 @@ public class MainActivity extends AppCompatActivity {
 
         final SimpleArcDialog mDialog = new SimpleArcDialog(MainActivity.this);
         mDialog.setConfiguration(new ArcConfiguration(MainActivity.this));
+        mDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+                findViewById(R.id.rootlayout).setVisibility(View.VISIBLE);
+            }
+        });
 
         findViewById(R.id.loaderButtonRandom).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                findViewById(R.id.rootlayout).setVisibility(View.INVISIBLE);
                 mDialog.show();
             }
         });
